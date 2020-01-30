0a73e9860664634aadfaebb3c2d71dca8f488fe8
Oleg Reshetilo <archdeuce@gmail.com>
2020-01-30 22:30:24 +0200

    2020-01-25_cw

---
+++ 2020-01-25_cw/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020_01_25_cw
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        public Customer(string firstName, string lastName, string city)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.City = city;
+        }
+
+        public Customer()
+        {
+        }
+    }
+}
---
+++ 2020-01-25_cw/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020_01_25_cw.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; set; }
+        public Customer SelectedCustomer { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.Customers = new ObservableCollection<Customer>() { 
+            
+                new Customer("Elena", "Zub", "Kharkiv"),
+                new Customer("Roman", "Sokolenko", "Kharkiv"),
+                new Customer("Tanya", "Ryzhik", "Kharkiv")
+            };
+        }
+    }
+}

ac352477ff31a10df01844a7b11ae413fa215ba9
Oleg Reshetilo <archdeuce@gmail.com>
2020-01-30 22:25:41 +0200

    ZzaDashboard

---
+++ ZzaDashboard/Model/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZzaDashboard.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+
+        public Customer(string firstName, string lastName, string phone)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Phone = phone;
+        }
+
+        public Customer()
+        {
+        }
+    }
+}
---
+++ ZzaDashboard/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ZzaDashboard.Model;
+
+namespace ZzaDashboard.View
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private Customer customer;
+        public Customer Customer 
+        {
+            get
+            {
+                return this.customer;
+            }
+            set
+            {
+                if (this.customer == value)
+                    return;
+
+                this.customer = value;
+                this.OnPropertyChanged(nameof(this.Customer));
+            } 
+        }
+
+        public ICommand ChangeCustomer { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged is null)
+                return;
+
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+        
+        public MainWindowViewModel()
+        {
+            this.Customer = new Customer("test1", "test2", "123");
+            this.ChangeCustomer = new RelayCommand(this.ChangeCustomerCommandExecute);
+        }
+
+        private void ChangeCustomerCommandExecute(object obj)
+        {
+            this.Customer = new Customer("test1313", "test2342", "444");
+        }
+    }
+}

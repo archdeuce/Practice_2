14a4d28ff222822f6168a75aa9e668c1ded0b3e2
Oleg Reshetilo <archdeuce@gmail.com>
2020-02-16 22:11:43 +0200

    2020-02-04_cw

---
+++ DbFirst/Models/BikeStoresContext.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DbFirst.Models
+{
+    public partial class BikeStoresContext : DbContext
+    {
+        public BikeStoresContext()
+        {
+        }
+
+        public BikeStoresContext(DbContextOptions<BikeStoresContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Brands> Brands { get; set; }
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<OldProducts> OldProducts { get; set; }
+        public virtual DbSet<OldProducts1> OldProducts1 { get; set; }
+        public virtual DbSet<OrderItems> OrderItems { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<Staffs> Staffs { get; set; }
+        public virtual DbSet<Stocks> Stocks { get; set; }
+        public virtual DbSet<Stores> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Database=BikeStores;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brands>(entity =>
+            {
+                entity.HasKey(e => e.BrandId)
+                    .HasName("PK__brands__5E5A8E27C593C24D");
+
+                entity.ToTable("brands");
+
+                entity.Property(e => e.BrandId).HasColumnName("brand_id");
+
+                entity.Property(e => e.BrandName)
+                    .IsRequired()
+                    .HasColumnName("brand_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Categories>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId)
+                    .HasName("PK__categori__D54EE9B4FFECB6F3");
+
+                entity.ToTable("categories");
+
+                entity.Property(e => e.CategoryId).HasColumnName("category_id");
+
+                entity.Property(e => e.CategoryName)
+                    .IsRequired()
+                    .HasColumnName("category_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Customers>(entity =>
+            {
+                entity.HasKey(e => e.CustomerId)
+                    .HasName("PK__customer__CD65CB85A3BBABED");
+
+                entity.ToTable("customers");
+
+                entity.Property(e => e.CustomerId).HasColumnName("customer_id");
+
+                entity.Property(e => e.City)
+                    .HasColumnName("city")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("first_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("last_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .HasColumnName("phone")
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.State)
+                    .HasColumnName("state")
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Street)
+                    .HasColumnName("street")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ZipCode)
+                    .HasColumnName("zip_code")
+                    .HasMaxLength(5)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<OldProducts>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("old_products");
+
+                entity.Property(e => e.BrandId).HasColumnName("brand_id");
+
+                entity.Property(e => e.CategoryId).HasColumnName("category_id");
+
+                entity.Property(e => e.ListPrice)
+                    .HasColumnName("list_price")
+                    .HasColumnType("decimal(10, 2)");
+
+                entity.Property(e => e.ModelYear).HasColumnName("model_year");
+
+                entity.Property(e => e.ProductId)
+                    .HasColumnName("product_id")
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasColumnName("product_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<OldProducts1>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("old_products1");
+
+                entity.Property(e => e.BrandId).HasColumnName("brand_id");
+
+                entity.Property(e => e.CategoryId).HasColumnName("category_id");
+
+                entity.Property(e => e.ListPrice)
+                    .HasColumnName("list_price")
+                    .HasColumnType("decimal(10, 2)");
+
+                entity.Property(e => e.ModelYear).HasColumnName("model_year");
+
+                entity.Property(e => e.ProductId)
+                    .HasColumnName("product_id")
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasColumnName("product_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<OrderItems>(entity =>
+            {
+                entity.HasKey(e => new { e.OrderId, e.ItemId })
+                    .HasName("PK__order_it__837942D461CC2C41");
+
+                entity.ToTable("order_items");
+
+                entity.Property(e => e.OrderId).HasColumnName("order_id");
+
+                entity.Property(e => e.ItemId).HasColumnName("item_id");
+
+                entity.Property(e => e.Discount)
+                    .HasColumnName("discount")
+                    .HasColumnType("decimal(4, 2)");
+
+                entity.Property(e => e.ListPrice)
+                    .HasColumnName("list_price")
+                    .HasColumnType("decimal(10, 2)");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderItems)
+                    .HasForeignKey(d => d.OrderId)
+                    .HasConstraintName("FK__order_ite__order__72C60C4A");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderItems)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__order_ite__produ__73BA3083");
+            });
+
+            modelBuilder.Entity<Orders>(entity =>
+            {
+                entity.HasKey(e => e.OrderId)
+                    .HasName("PK__orders__465962298FCA5BCC");
+
+                entity.ToTable("orders");
+
+                entity.Property(e => e.OrderId).HasColumnName("order_id");
+
+                entity.Property(e => e.CustomerId).HasColumnName("customer_id");
+
+                entity.Property(e => e.OrderDate)
+                    .HasColumnName("order_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.OrderStatus).HasColumnName("order_status");
+
+                entity.Property(e => e.RequiredDate)
+                    .HasColumnName("required_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.ShippedDate)
+                    .HasColumnName("shipped_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.StaffId).HasColumnName("staff_id");
+
+                entity.Property(e => e.StoreId).HasColumnName("store_id");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK__orders__customer__6D0D32F4");
+
+                entity.HasOne(d => d.Staff)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.StaffId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__orders__staff_id__6EF57B66");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.StoreId)
+                    .HasConstraintName("FK__orders__store_id__6E01572D");
+            });
+
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasKey(e => e.ProductId)
+                    .HasName("PK__products__47027DF5C1B44220");
+
+                entity.ToTable("products");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.BrandId).HasColumnName("brand_id");
+
+                entity.Property(e => e.CategoryId).HasColumnName("category_id");
+
+                entity.Property(e => e.ListPrice)
+                    .HasColumnName("list_price")
+                    .HasColumnType("decimal(10, 2)");
+
+                entity.Property(e => e.ModelYear).HasColumnName("model_year");
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasColumnName("product_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Brand)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.BrandId)
+                    .HasConstraintName("FK__products__brand___619B8048");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK__products__catego__60A75C0F");
+            });
+
+            modelBuilder.Entity<Staffs>(entity =>
+            {
+                entity.HasKey(e => e.StaffId)
+                    .HasName("PK__staffs__1963DD9C14433EEC");
+
+                entity.ToTable("staffs");
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("UQ__staffs__AB6E6164F4889592")
+                    .IsUnique();
+
+                entity.Property(e => e.StaffId).HasColumnName("staff_id");
+
+                entity.Property(e => e.Active).HasColumnName("active");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("first_name")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("last_name")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ManagerId).HasColumnName("manager_id");
+
+                entity.Property(e => e.Phone)
+                    .HasColumnName("phone")
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StoreId).HasColumnName("store_id");
+
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.InverseManager)
+                    .HasForeignKey(d => d.ManagerId)
+                    .HasConstraintName("FK__staffs__manager___6A30C649");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Staffs)
+                    .HasForeignKey(d => d.StoreId)
+                    .HasConstraintName("FK__staffs__store_id__693CA210");
+            });
+
+            modelBuilder.Entity<Stocks>(entity =>
+            {
+                entity.HasKey(e => new { e.StoreId, e.ProductId })
+                    .HasName("PK__stocks__E68284D3541166B5");
+
+                entity.ToTable("stocks");
+
+                entity.Property(e => e.StoreId).HasColumnName("store_id");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Stocks)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__stocks__product___778AC167");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Stocks)
+                    .HasForeignKey(d => d.StoreId)
+                    .HasConstraintName("FK__stocks__store_id__76969D2E");
+            });
+
+            modelBuilder.Entity<Stores>(entity =>
+            {
+                entity.HasKey(e => e.StoreId)
+                    .HasName("PK__stores__A2F2A30C7DBCA19D");
+
+                entity.ToTable("stores");
+
+                entity.Property(e => e.StoreId).HasColumnName("store_id");
+
+                entity.Property(e => e.City)
+                    .HasColumnName("city")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .HasColumnName("phone")
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.State)
+                    .HasColumnName("state")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StoreName)
+                    .IsRequired()
+                    .HasColumnName("store_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Street)
+                    .HasColumnName("street")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ZipCode)
+                    .HasColumnName("zip_code")
+                    .HasMaxLength(5)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DbFirst/Models/OldProducts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirst.Models
+{
+    public partial class OldProducts
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+        public short ModelYear { get; set; }
+        public decimal ListPrice { get; set; }
+    }
+}

095c16a53b46ba68dab7c1cf34c007af64793e4b
Oleg Reshetilo <archdeuce@gmail.com>
2020-02-16 22:19:43 +0200

    2020-02-11_cw

---
+++ 2020-02-11_cw/Models/SchoolContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2020_02_11_cw.Models
+{
+    public class SchoolContext: DbContext
+    {
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Database = SchoolDB; Trusted_Connection = True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasOne<Grade>(s => s.Grade)
+                .WithMany(g => g.Students)
+                .HasForeignKey(s => s.CurrentGradeId);
+
+            modelBuilder.Entity<Student>()
+                .HasOne<StudentAddress>(s => s.Address)
+                .WithOne(ad => ad.Student)
+                .HasForeignKey<StudentAddress>(ad => ad.AdressOfStudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+        }
+    }
+}
---
+++ 2020-02-11_cw/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2020_02_11_cw.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CurrentGradeId { get; set; }
+        public Grade Grade { get; set; }
+
+        public StudentAddress Address { get; set; }
+
+        public IList<StudentCourse> StudentCourses { get; set; }
+    }
+}
